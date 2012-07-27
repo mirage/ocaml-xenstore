6abba99e271604dd38e10024dc426ca5a60e3ec9
David Scott <dave.scott@eu.citrix.com>
2012-07-27 13:36:12 +0100

    test: add tests for response marshalling

--- lib_test/xs_test.ml
+++ lib_test/xs_test.ml
@@ -17,6 +17,10 @@ open OUnit
 let ( |> ) a b = b a
 let id x = x
 
+let unbox = function
+	| None -> failwith "unbox"
+	| Some x -> x
+
 let op_ids _ =
   let open Xs_packet.Op in
   for i = 0 to 100 do (* higher than the highest ID *)
@@ -72,54 +76,98 @@ let test _ =
   Lwt_main.run t
 
 type example_packet = {
-	description: string;
+	op: Xs_packet.Op.t;
 	packet: Xs_packet.t;
 	wire_fmt: string;
 }
-let make_example_request description pkt_opt wire_fmt = match pkt_opt with
-	| None -> failwith (Printf.sprintf "make_example_request:%s" description)
+let make_example_request op pkt_opt wire_fmt = match pkt_opt with
+	| None -> failwith (Printf.sprintf "make_example_request:%s" (Xs_packet.Op.to_string op))
 	| Some x -> {
-		description = description;
+		op = op;
 		packet = x;
 		wire_fmt = wire_fmt;
 	}
 
 let example_request_packets =
+	let open Xs_packet.Op in
     let open Xs_packet.Request in [
-		make_example_request "directory" (directory "/whatever/whenever" 5l)
+		make_example_request Directory (directory "/whatever/whenever" 5l)
 			"\x01\x00\x00\x00\x0f\x00\x00\x00\x05\x00\x00\x00\x13\x00\x00\x00\x2f\x77\x68\x61\x74\x65\x76\x65\x72\x2f\x77\x68\x65\x6e\x65\x76\x65\x72\x00";
-		make_example_request "read" (read "/a/b/c" 6l)
+		make_example_request Read (read "/a/b/c" 6l)
 			"\x02\x00\x00\x00\x0e\x00\x00\x00\x06\x00\x00\x00\x07\x00\x00\x00\x2f\x61\x2f\x62\x2f\x63\x00";
-		make_example_request "getperms" (getperms "/a/b" 7l)
+		make_example_request Getperms (getperms "/a/b" 7l)
 			"\x03\x00\x00\x00\x0d\x00\x00\x00\x07\x00\x00\x00\x05\x00\x00\x00\x2f\x61\x2f\x62\x00";
-		make_example_request "rm" (rm "/" 0l)
+		make_example_request Rm (rm "/" 0l)
 			"\x0d\x00\x00\x00\x0c\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x2f\x00";
-		make_example_request "setperms" (setperms "/" "someperms" 1l)
+		make_example_request Setperms (setperms "/" "someperms" 1l)
 			"\x0e\x00\x00\x00\x0b\x00\x00\x00\x01\x00\x00\x00\x0c\x00\x00\x00\x2f\x00\x73\x6f\x6d\x65\x70\x65\x72\x6d\x73\x00";
-		make_example_request "write" (write "/key" "value" 1l)
+		make_example_request Write (write "/key" "value" 1l)
 			"\x0b\x00\x00\x00\x0a\x00\x00\x00\x01\x00\x00\x00\x0a\x00\x00\x00\x2f\x6b\x65\x79\x00\x76\x61\x6c\x75\x65";
-		make_example_request "mkdir" (mkdir "/" 1024l)
+		make_example_request Mkdir (mkdir "/" 1024l)
 			"\x0c\x00\x00\x00\x09\x00\x00\x00\x00\x04\x00\x00\x02\x00\x00\x00\x2f\x00";
-		make_example_request "transaction_start" (transaction_start ())
+		make_example_request Transaction_start (transaction_start ())
 			"\x06\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00";
-		make_example_request "transaction_end" (transaction_end true 1l)
+		make_example_request Transaction_end (transaction_end true 1l)
 			"\x07\x00\x00\x00\x07\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x54\x00";
-		make_example_request "introduce" (introduce 4 5n 1)
+		make_example_request Introduce (introduce 4 5n 1)
 			"\x08\x00\x00\x00\x06\x00\x00\x00\x00\x00\x00\x00\x06\x00\x00\x00\x34\x00\x35\x00\x31\x00";
-		make_example_request "release" (release 2)
+		make_example_request Release (release 2)
 			"\x09\x00\x00\x00\x05\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x32\x00";
-		make_example_request "resume" (resume 3)
+		make_example_request Resume (resume 3)
 			"\x12\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x33\x00";
-		make_example_request "getdomainpath" (getdomainpath 3)
+		make_example_request Getdomainpath (getdomainpath 3)
 			"\x0a\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x33\x00";
-		make_example_request "watch" (watch "/foo/bar" (Xs_packet.Token.of_user_string "something"))
+		make_example_request Watch (watch "/foo/bar" (Xs_packet.Token.of_user_string "something"))
 			"\x04\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x15\x00\x00\x00\x2f\x66\x6f\x6f\x2f\x62\x61\x72\x00\x31\x3a\x73\x6f\x6d\x65\x74\x68\x69\x6e\x67\x00";
-		make_example_request "unwatch" (unwatch "/foo/bar" (Xs_packet.Token.of_user_string "somethinglse"))
+		make_example_request Unwatch (unwatch "/foo/bar" (Xs_packet.Token.of_user_string "somethinglse"))
 			"\x05\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x18\x00\x00\x00\x2f\x66\x6f\x6f\x2f\x62\x61\x72\x00\x30\x3a\x73\x6f\x6d\x65\x74\x68\x69\x6e\x67\x6c\x73\x65\x00";
-		make_example_request "debug" (debug [ "a"; "b"; "something" ])
+		make_example_request Debug (debug [ "a"; "b"; "something" ])
 			"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0e\x00\x00\x00\x61\x00\x62\x00\x73\x6f\x6d\x65\x74\x68\x69\x6e\x67\x00"
 	]
 
+let make_example_response op f wire_fmt =
+	let request = List.find (fun x -> x.op = op) example_request_packets in {
+		op = op;
+		packet = f request.packet;
+		wire_fmt = wire_fmt;
+	}
+
+(* We use the example requests to generate example responses *)
+let example_response_packets =
+	let open Xs_packet.Op in
+	let open Xs_packet.Response in [
+		make_example_response Read (fun t -> read t "theresult")
+			"\x02\x00\x00\x00\x0e\x00\x00\x00\x06\x00\x00\x00\x0a\x00\x00\x00\x74\x68\x65\x72\x65\x73\x75\x6c\x74\x00";
+		make_example_response Getperms (fun t -> getperms t (2, Xs_packet.ACL.READ, [ 4, Xs_packet.ACL.NONE ]))
+			"\x03\x00\x00\x00\x0d\x00\x00\x00\x07\x00\x00\x00\x06\x00\x00\x00\x72\x32\x00\x6e\x34\x00";
+		make_example_response Getdomainpath (fun t -> getdomainpath t "/local/domain/4")
+			"\x0a\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x00\x2f\x6c\x6f\x63\x61\x6c\x2f\x64\x6f\x6d\x61\x69\x6e\x2f\x34\x00";
+		make_example_response Transaction_start (fun t -> transaction_start t 3l)
+			"\x06\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x33\x00";
+		make_example_response Directory (fun t -> directory t [ "a"; "b"; "c"; "aseasyas"; "1"; "2"; "3" ])
+			"\x01\x00\x00\x00\x0f\x00\x00\x00\x05\x00\x00\x00\x15\x00\x00\x00\x61\x00\x62\x00\x63\x00\x61\x73\x65\x61\x73\x79\x61\x73\x00\x31\x00\x32\x00\x33\x00";
+		make_example_response Write write
+			"\x0b\x00\x00\x00\x0a\x00\x00\x00\x01\x00\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		make_example_response Mkdir mkdir
+			"\x0c\x00\x00\x00\x09\x00\x00\x00\x00\x04\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		make_example_response Rm rm
+			"\x0d\x00\x00\x00\x0c\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		make_example_response Setperms setperms
+			"\x0e\x00\x00\x00\x0b\x00\x00\x00\x01\x00\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		make_example_response Watch watch
+			"\x04\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		make_example_response Unwatch unwatch
+			"\x05\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		make_example_response Transaction_end transaction_end
+			"\x07\x00\x00\x00\x07\x00\x00\x00\x01\x00\x00\x00\x03\x00\x00\x00\x4f\x4b\x00";
+		{
+			op = Error;
+			packet = error (Xs_packet.Request.directory "/foo" 2l |> unbox) "whatyoutalkingabout";
+			wire_fmt = "\x01\x00\x00\x00\x10\x00\x00\x00\x02\x00\x00\x00\x14\x00\x00\x00\x77\x68\x61\x74\x79\x6f\x75\x74\x61\x6c\x6b\x69\x6e\x67\x61\x62\x6f\x75\x74\x00"
+		}
+	]
+
+let example_packets = example_request_packets @ example_response_packets
 
 let rec ints first last =
 	if first > last then [] else first :: (ints (first + 1) last)
@@ -145,25 +193,26 @@ let _ =
   ] (fun x -> Printf.fprintf stderr "Ignoring argument: %s" x)
     "Test xenstore protocol code";
 
-  let request_parsing choose =
+  let packet_parsing choose =
     let f = test_packet_parser choose in
-    let open Xs_packet.Request in
-    "request parsing" >:::
+    "packet_parsing" >:::
 		(List.map (fun example ->
-			example.description >:: f example.packet
-		) example_request_packets) in
-  let request_printing =
-	  "request_printing" >:::
+			let description = Xs_packet.Op.to_string example.op in
+			description >:: f example.packet
+		) example_packets) in
+  let packet_printing =
+	  "packet_printing" >:::
 		  (List.map (fun example ->
-			  example.description >:: (fun () -> assert_equal ~msg:example.description ~printer:hexstring (Xs_packet.to_string example.packet) example.wire_fmt)
-		  ) example_request_packets) in
+			  let description = Xs_packet.Op.to_string example.op in
+			  description >:: (fun () -> assert_equal ~msg:description ~printer:hexstring (Xs_packet.to_string example.packet) example.wire_fmt)
+		  ) example_packets) in
   let suite = "xenstore" >:::
     [
       "op_ids" >:: op_ids;
       "acl_parser" >:: acl_parser;
-      request_parsing id;
-      request_parsing (fun _ -> 1);
-	  request_printing;
+      packet_parsing id;
+      packet_parsing (fun _ -> 1);
+	  packet_printing;
       "test" >:: test;
     ] in
   run_test_tt ~verbose:!verbose suite
