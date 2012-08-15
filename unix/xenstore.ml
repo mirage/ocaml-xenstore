47f397ac9de982a5f6eedf84427899eaf5368e29
David Scott <dave.scott@eu.citrix.com>
2012-08-15 11:56:53 +0100

    Fix the build

--- unix/xenstore.ml
+++ unix/xenstore.ml
@@ -1,4 +1,4 @@
 
-external map_foreign_job: int -> nativeint -> Cstruct.buf job = "lwt_map_foreign_job"
+external map_foreign_job: int -> nativeint -> Cstruct.buf Lwt_unix.job = "lwt_map_foreign_job"
 
 let map_foreign domid mfn = Lwt_unix.run_job (map_foreign_job domid mfn)
