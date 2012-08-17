e1beb860db8fcd90e9be5e9904e9722d5f57e5a2
David Scott <dave.scott@eu.citrix.com>
2012-08-17 15:28:35 +0100

    store: add a memory interface to query the current memory usage

--- server/call.ml
+++ server/call.ml
@@ -35,6 +35,8 @@ let get_namespace_implementation path = match Store.Path.to_string_list path wit
 		Store.Path.of_string_list rest, (module Connection.Interface: Namespace.IO)
 	| "log" :: rest ->
 		Store.Path.of_string_list rest, (module Logging_interface: Namespace.IO)
+	| "memory" :: rest ->
+		Store.Path.of_string_list rest, (module Memory_interface: Namespace.IO)
 	| _ ->
 		path, (module Transaction: Namespace.IO)
 
--- server/xs_server.ml
+++ server/xs_server.ml
@@ -28,7 +28,7 @@ let store =
 			let p = Store.Path.create path (Store.Path.getdomainpath 0) in
 			if not (Store.exists store p)
 			then Store.mkdir store 0 (Perms.of_domain 0) p
-		) [ "/local"; "/local/domain"; "/quota"; "/connection"; "/log" ];
+		) [ "/local"; "/local/domain"; "/quota"; "/connection"; "/log"; "/memory" ];
 	store
 
 module type TRANSPORT = sig
