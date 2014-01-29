8e2488fdad6288c8614dd0c4ffb8a22774db6934
David Scott <dave.scott@eu.citrix.com>
2014-01-29 16:04:25 +0000

    userspace: use the standard xen-evtchn package
    Remove the in-tree bindings.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- userspace/xenstore.mli
+++ userspace/xenstore.mli
@@ -12,22 +12,3 @@ type info = {
 }
 
 val domain_infolist: unit -> info list option Lwt.t
-
-type xc_evtchn
-val xc_evtchn_open: unit -> xc_evtchn option
-
-val xc_evtchn_close: destroy: xc_evtchn -> unit
-
-val xc_evtchn_fd: xc_evtchn -> Unix.file_descr option
-
-val xc_evtchn_notify: xc_evtchn -> int -> unit
-
-val xc_evtchn_bind_interdomain: xc_evtchn -> int -> int -> int option
-
-val xc_evtchn_bind_virq_dom_exc: xc_evtchn -> int option
-
-val xc_evtchn_unbind: xc_evtchn -> int -> unit
-
-val xc_evtchn_pending: xc_evtchn -> int option
-
-val xc_evtchn_unmask: xc_evtchn -> int -> unit
