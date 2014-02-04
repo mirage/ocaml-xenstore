29f3aa30e75fc6d749330e45594d185e31c32ab2
David Scott <dave.scott@eu.citrix.com>
2014-02-04 17:43:50 +0000

    Add license header to domains.ml
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- userspace/domains.ml
+++ userspace/domains.ml
@@ -1,4 +1,16 @@
-
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
 
 external map_foreign: int -> nativeint -> Io_page.t = "stub_map_foreign"
 external unmap_foreign: Io_page.t -> unit           = "stub_unmap_foreign"
